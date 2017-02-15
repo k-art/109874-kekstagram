b401bee88a0a8993c0a5d5f0cfb747702161708b
Artem Kozlov <arte.horn@gmail.com>
2017-02-15 17:37:22 +0100

    add encapsulation

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,51 +1,53 @@
 'use strict';
 
-var upload = document.querySelector('.upload');
-var uploadOverlay = upload.querySelector('.upload-overlay');
-var filterImagePreview = document.querySelector('.filter-image-preview');
-var resizeButtonDec = uploadOverlay.querySelector('.upload-resize-controls-button-dec');
-var resizeButtonInc = uploadOverlay.querySelector('.upload-resize-controls-button-inc');
-var resizeControlsField = uploadOverlay.querySelector('.upload-resize-controls-value');
-
 // Изменение масштаба изображения
-window.createScale = function (element, step, initScale) {
+window.createScale = (function () {
+  var upload = document.querySelector('.upload');
+  var uploadOverlay = upload.querySelector('.upload-overlay');
+  var filterImagePreview = document.querySelector('.filter-image-preview');
+  var resizeButtonDec = uploadOverlay.querySelector('.upload-resize-controls-button-dec');
+  var resizeButtonInc = uploadOverlay.querySelector('.upload-resize-controls-button-inc');
+  var resizeControlsField = uploadOverlay.querySelector('.upload-resize-controls-value');
 
-  var MIN_VALUE = 25;
-  var MAX_VALUE = 100;
-  var STEP = step;
+  return function (element, step, initScale) {
 
-  resizeControlsField.setAttribute('value', initScale);
-  filterImagePreview.style.transform = 'scale(' + parseInt(initScale, 10) / 100 + ')';
-  filterImagePreview.style.msTransform = 'scale(' + parseInt(initScale, 10) / 100 + ')';
-  filterImagePreview.style.webkitTransform = 'scale(' + parseInt(initScale, 10) / 100 + ')';
+    var MIN_VALUE = 25;
+    var MAX_VALUE = 100;
+    var STEP = step;
 
-  element.addEventListener('click', function (event) {
-    var target = event.target;
+    resizeControlsField.setAttribute('value', initScale);
+    filterImagePreview.style.transform = 'scale(' + parseInt(initScale, 10) / 100 + ')';
+    filterImagePreview.style.msTransform = 'scale(' + parseInt(initScale, 10) / 100 + ')';
+    filterImagePreview.style.webkitTransform = 'scale(' + parseInt(initScale, 10) / 100 + ')';
 
-    if (target.tagName.toLowerCase() === 'button' && target.classList.contains('upload-resize-controls-button')) {
-      var currentButton = target;
-    }
+    element.addEventListener('click', function (event) {
+      var target = event.target;
 
-    var currentValue = parseInt(resizeControlsField.getAttribute('value'), 10);
-
-    if (currentButton === resizeButtonDec && currentValue !== MIN_VALUE) {
-      currentValue -= STEP;
-      if (currentValue < MIN_VALUE) {
-        currentValue = MIN_VALUE;
+      if (target.tagName.toLowerCase() === 'button' && target.classList.contains('upload-resize-controls-button')) {
+        var currentButton = target;
       }
-    }
 
-    if (currentButton === resizeButtonInc && currentValue !== MAX_VALUE) {
-      currentValue += STEP;
-      if (currentValue > MAX_VALUE) {
-        currentValue = MAX_VALUE;
+      var currentValue = parseInt(resizeControlsField.getAttribute('value'), 10);
+
+      if (currentButton === resizeButtonDec && currentValue !== MIN_VALUE) {
+        currentValue -= STEP;
+        if (currentValue < MIN_VALUE) {
+          currentValue = MIN_VALUE;
+        }
       }
-    }
-    var scaleValue = currentValue / 100;
 
-    resizeControlsField.setAttribute('value', currentValue + '%');
-    filterImagePreview.style.transform = 'scale(' + scaleValue + ')';
-    filterImagePreview.style.msTransform = 'scale(' + scaleValue + ')';
-    filterImagePreview.style.webkitTransform = 'scale(' + scaleValue + ')';
-  });
-};
+      if (currentButton === resizeButtonInc && currentValue !== MAX_VALUE) {
+        currentValue += STEP;
+        if (currentValue > MAX_VALUE) {
+          currentValue = MAX_VALUE;
+        }
+      }
+      var scaleValue = currentValue / 100;
+
+      resizeControlsField.setAttribute('value', currentValue + '%');
+      filterImagePreview.style.transform = 'scale(' + scaleValue + ')';
+      filterImagePreview.style.msTransform = 'scale(' + scaleValue + ')';
+      filterImagePreview.style.webkitTransform = 'scale(' + scaleValue + ')';
+    });
+  };
+})();
